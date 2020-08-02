4477c66d9027064e4939a8cadeaa6612f7697ec0
Erik van Dam <erik.vandam@trivago.com>
2020-08-02 23:27:41 +0200

    Added consul and ambassador resolver and registered the first service

--- .devops/terraform/charts.tf
+++ .devops/terraform/charts.tf
@@ -10,6 +10,12 @@ resource "kubernetes_namespace" "ambassador" {
   }
 }
 
+resource "kubernetes_namespace" "services" {
+  metadata {
+    name = "services"
+  }
+}
+
 resource "helm_release" "consul" {
   name       = "${local.prefix}-consul"
   repository = "https://helm.releases.hashicorp.com"
@@ -74,4 +80,45 @@ resource "helm_release" "ambassador" {
     value = "true"
     type  = "string"
   }
+}
+
+resource "helm_release" "ambassador_consul_resolver" {
+  name      = "${local.prefix}-ambassador-consul-resolver"
+  chart     = "./helm-charts/ambassador-consul-resolver"
+  namespace = "ambassador"
+
+  depends_on = [
+    helm_release.ambassador,
+  ]
+
+  set {
+    name  = "resolver.name"
+    value = "consul-dc1"
+  }
+
+  set {
+    name  = "consul.host"
+    value = "consul.service.consul"
+  }
+
+  set {
+    name  = "consul.port"
+    value = "8500"
+  }
+
+  set {
+    name  = "consul.datacenter"
+    value = "dc1"
+  }
+}
+
+resource "helm_release" "hello_world" {
+  name      = "${var.prefix}-hello-world-service"
+  chart     = "./helm-charts/hello-world-service"
+  namespace = "services"
+
+  depends_on = [
+    helm_release.ambassador,
+    helm_release.consul,
+  ]
 }
