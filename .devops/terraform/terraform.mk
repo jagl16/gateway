e6408adb6570e11110a5af87561536ae9076e116
Erik van Dam <erik.vandam@trivago.com>
2020-08-02 14:15:14 +0200

    Added setup for Terraform

---
+++ .devops/terraform/terraform.mk
@@ -0,0 +1,31 @@
+.PHONY: tf-init
+tf-init:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform init
+
+.PHONY: tf-fmt
+tf-fmt:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform fmt -recursive
+
+.PHONY: tf-validate
+tf-validate:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform validate
+
+.PHONY: tf-plan
+tf-plan:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform plan
+
+.PHONY: tf-apply
+tf-apply:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform apply
+
+.PHONY: tf-approved-apply
+tf-approved-apply:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform apply -auto-approve
+
+.PHONY: tf-destroy
+tf-destroy:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform destroy
+
+.PHONY: tf-destroy
+tf-current-workspace:
+	docker-compose -f .devops/terraform/docker-compose.yml run --rm terraform workspace show
