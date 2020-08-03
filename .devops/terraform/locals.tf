a1183ae3b919d67869881c4f648ec7fa7a39f708
Erik van Dam <erik.vandam@trivago.com>
2020-08-03 16:07:02 +0200

    Added missing staging locals

--- .devops/terraform/locals.tf
+++ .devops/terraform/locals.tf
@@ -3,20 +3,21 @@ locals {
 
   cluster_name = "${local.prefix}-cluster"
 
-  prefix = lookup({
-    default = "${local.environment}-${var.prefix}"
-  }, local.environment)
+  prefix = "${local.environment}-${var.prefix}"
 
   vpc_cidr = lookup({
     default = "10.0.0.0/16"
+    staging = "172.0.0.0/16"
   }, local.environment)
 
   vpc_public_subnets = lookup({
     default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    staging = cidrsubnets(local.vpc_cidr, 1, 2, 3)
   }, local.environment)
 
   vpc_private_subnets = lookup({
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    staging = cidrsubnets(local.vpc_cidr, 4, 5, 6)
   }, local.environment)
 
   common_tags = {
