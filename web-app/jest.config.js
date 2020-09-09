a094cddc4baabf76de99e6d3538c652186e5d63b
Erik van Dam <erik.vandam@trivago.com>
2020-09-09 13:08:53 +0200

    Added next.js

--- web-app/jest.config.js
+++ web-app/jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  testMatch: ["<rootDir>/src/**/?(*.)test.ts?(x)"],
 };
--- web-app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { HelloWorld } from "./components/HelloWorld";
-
-export function App(): JSX.Element {
-  return <HelloWorld/>;
-}
