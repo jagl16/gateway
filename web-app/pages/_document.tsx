ba60dec09b1a54d835dabf6d3d79c4a8a891037c
Erik van Dam <erik.vandam@trivago.com>
2020-09-10 11:45:15 +0200

    Fixed a bug in _document as it does not allow passing an anonymous function

--- web-app/pages/_document.tsx
+++ web-app/pages/_document.tsx
@@ -3,24 +3,25 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { cn } from "@scaling/utils";
 
 class Document extends NextDocument {
-    render = (): JSX.Element => (
-        <Html className={cn("h-full", "w-full")} lang="en">
-            <Head/>
-            <body
-                className={cn("w-full", "h-full", "font-sans", "text-black", "font-thin", "antialiased", "text-black")}>
-            <Main/>
-            <NextScript/>
+    render(): JSX.Element {
+        return (
+            <Html className={cn("h-full", "w-full")} lang="en">
+                <Head />
+                <body className={cn("w-full" ,"h-full", "font-sans", "text-black", "font-thin", "antialiased", "text-black")}>
+                    <Main />
+                    <NextScript />
 
-            <style jsx global>{`
-                #__next {
-                    width: 100%;
-                    height: 100%;
-                }
-            `}
-            </style>
-            </body>
-        </Html>
-    );
+                    <style jsx global={true}>{`
+                        #__next {
+                            width: 100%;
+                            height: 100%;
+                        }
+                    `}
+                    </style>
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default Document
