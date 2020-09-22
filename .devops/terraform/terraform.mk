4c9ae0559eff88c91beb0594557a951498704913
Erik van Dam <erik.vandam@trivago.com>
2020-09-22 22:53:34 +0200

    workflow(terraform): Added init make rule

--- .devops/terraform/terraform.mk
+++ .devops/terraform/terraform.mk
@@ -1,4 +1,4 @@
-tf-plan-prod	tf-apply-prod	tf-state-prod	tf-destroy-prod: TERRAFORMWORKDIR=terraform/environments/production
+tf-init-prod	tf-plan-prod	tf-apply-prod	tf-state-prod	tf-destroy-prod: TERRAFORMWORKDIR=terraform/environments/production
 
 TERRAFORM_IMAGE := hashicorp/terraform@sha256:691e2f368183a1886b50fd7da16b4511f5ac914ff6b7c748a87a37e84b898c50
 
@@ -23,14 +23,16 @@ tf-validate: ## Invokes terraform validate command
 	${TERRAFORM} validate
 	${TERRAFORM} fmt -check
 
+.PHONY: tf-init-%
+tf-init-%: ## Invokes terraform plan command
+	${TERRAFORM} init
+
 .PHONY: tf-plan-%
 tf-plan-%: ## Invokes terraform plan command
-	${TERRAFORM} init
 	${TERRAFORM} plan
 
 .PHONY: tf-apply-%
 tf-apply-%: ## Invokes terraform apply command
-	${TERRAFORM} init
 	${TERRAFORM} apply -auto-approve
 
 .PHONY: tf-state-%
