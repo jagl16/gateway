892dc98c4257b7318060160e92adab67b0d5b642
Erik van Dam <erik.vandam@trivago.com>
2020-09-22 11:19:33 +0200

    fix(terraform): Remove unused modules

--- .devops/terraform/environments/production/variables.tf
+++ .devops/terraform/environments/production/variables.tf
@@ -10,10 +10,6 @@ variable "root_domain" {
   default = "scaling.cloud"
 }
 
-variable "app_domain" {
-  default = "app.scaling.cloud"
-}
-
 variable "api_domain" {
   default = "api.scaling.cloud"
 }
@@ -25,18 +21,6 @@ variable "resolver_name" {
 variable "use_existing_route53_zone" {
   type    = bool
   default = true
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "vpc_public_subnets" {
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
-variable "vpc_private_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "common_tags" {
